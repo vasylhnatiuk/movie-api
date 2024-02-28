d6532a88d05b939edb67e8e031da101e9608c116
vasylhnatiuk <vasylhnatiukjob@gmail.com>
2024-02-28 16:11:10 +0100

    added new allowed host

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "movie-api-71ja.onrender.com"]
 
 # Application definition
 
